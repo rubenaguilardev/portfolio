794362ed79390645afa9822cb84f1c050ac6940f
rubenaguilardev <ragui56@wgu.edu>
2025-07-17 15:02:30 -0700

    create project, add component files

---
+++ src/LoadingScreen.tsx
@@ -0,0 +1,8 @@
+export const LoadingScreen = () => {
+    return (
+        <div className='fixed flex flex-col justify-center items-center inset-0 z-50 bg-black text-gray-100'>
+            <div className='mb-4 text-4xl font-mono font-bold'>
+                Hello World <span className=""></span></div>
+        </div>
+    )
+}
