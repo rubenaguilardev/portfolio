d90ba3e522cd0aa8f878e45b0911ee22cda3661f
Ruben Aguilar <ragui56@wgu.edu>
2025-12-28 02:23:07 -0800

    add navbar links

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,6 +1,29 @@
 const Navbar = () => {
+
+    const navLinks = [
+        { href: '#about', label: 'About' },
+        { href: '#projects', label: 'Projects' },
+        { href: '#experience', label: 'Experience' },
+        { href: '#testimonials', label: 'Testimonials' },
+        { href: '#contact', label: 'Contact' },
+    ]
+
     return (
-        <header></header>
+        <header>
+            <nav>
+                <a href="">RA<span>.</span></a>
+
+                <div>
+                    <div>
+                        {navLinks.map((link, index) => (
+                            <a href={link.href} key={index}>
+                                {link.label}
+                            </a>
+                        ))}
+                    </div>
+                </div>
+            </nav>
+        </header>
     )
 }
 
