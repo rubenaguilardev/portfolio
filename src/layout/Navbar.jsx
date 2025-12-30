440d34792dd05cf48fedd885a337782fe812f482
Ruben Aguilar <ragui56@wgu.edu>
2025-12-30 01:44:20 -0800

    add mobile navbar

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,6 +1,10 @@
 import Button from "@/components/Button"
+import { Menu, X } from "lucide-react"
+import { useState } from "react"
 
 const Navbar = () => {
+
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
 
     const navLinks = [
         { href: '#about', label: 'About' },
@@ -16,7 +20,7 @@ const Navbar = () => {
                     RA<span className="text-primary">.</span>
                 </a>
 
-                <div className="flex items-center gap-1">
+                <div className="hidden md:flex items-center gap-1">
                     <div className="glass rounded-full px-2 py-1 flex items-center gap-1">
                         {navLinks.map((link, index) => (
                             <a 
@@ -30,10 +34,36 @@ const Navbar = () => {
                     </div>
                 </div>
 
-                <div>
+                <div className="hidden md:block">
                     <Button size='sm'>Contact Me</Button>
                 </div>
+
+                <button 
+                    className="md:hidden p-2 text-foreground cursor-pointer"
+                    onClick={() => setIsMobileMenuOpen(prev => !prev)}
+                >
+                    {isMobileMenuOpen ? <X size={24}/> : <Menu size={24}/>}
+                </button>
             </nav>
+
+
+            { isMobileMenuOpen && (<div className="md:hidden glass-strong animate-fade-in">
+                <div className="container mx-auto px-6 py-6 flex flex-col">
+                    {navLinks.map((link, index) => (
+                        <a 
+                            href={link.href} 
+                            key={index} 
+                            className="text-lg text-muted-foreground hover:text-foreground py-2"
+                        >
+                            {link.label}
+                        </a>
+                    ))}
+
+                    <Button className='mt-4'>Contact Me</Button>
+
+                    </div>
+                </div>
+            )}
         </header>
     )
 }
